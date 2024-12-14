2f63eb5c59b16a63f56a03b63623cacbf9803d38
aquino <jaquinox123@gmail.com>
2024-12-14 15:24:46 -0400

    ¨´´

---
+++ src/components/MyApp01.vue
@@ -0,0 +1,24 @@
+<script>
+
+</script>
+
+<template>
+
+    <div>        Mi primera App en <spam>Vue.js</spam> </div>
+
+</template>
+
+
+<style scoped>
+div{
+    color: #35495f;
+    font-size: 1.6rem ;
+
+}
+
+spam {
+color: aqua;
+font-size: 50px;
+}
+
+</style>
